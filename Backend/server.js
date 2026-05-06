b8f5bb1811e53bd91778c1304227e1a7c53b169f
Nandini Dubey <nandinidubey631483@gmai.com>
2026-05-06 17:35:01 +0530

    added home route

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,11 @@ const connectToDB = require("./src/config/database")
 
 
 connectToDB();
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 const PORT = process.env.PORT || 3000;
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
